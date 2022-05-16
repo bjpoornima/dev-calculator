1633d6259b6e8a20d4aaab090e39d98a66d1c010
B J Poornima <b.j.poornima@sap.com>
2022-05-16 19:17:41 +0530

    Initial commit

---
+++ src/main/java/devops/Calculator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package devops;
+
+/**
+ * @author 23040
+ *
+ */
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
